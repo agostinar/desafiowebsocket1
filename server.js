6d8cd929d9cc4a75ab6968a0ad907f647a58cd4a
agostinar <132631770+agostinar@users.noreply.github.com>
2023-08-26 13:51:36 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+POST / HTTP/1.1
+Host: localhost:8080
+Content-Type: application/json
+
+{
+    "id": 2,
+    "title": "Nuevo producto",
+    "description": "Descripción del nuevo producto",
+    "code": "new123",
+    "price": 100,
+    "status": true,
+    "stock": 20,
+    "category": "Electronics"
+}
